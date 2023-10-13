93ae934649d4d23b09be296f309e2741d4aaeefd
Thomas Petty <146855548+Petty-Exploits@users.noreply.github.com>
2023-10-13 14:29:40 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tabLimitInput = document.getElementById("tabLimit");
     const setLimitButton = document.getElementById("setLimitButton");
-    const darkModeToggle = document.getElementById("darkModeToggle"); // Add this line
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const saveMessage = document.getElementById("saveMessage"); // Get the "Changes saved" message element
 
     // Load the current tab limit from storage and display it
     chrome.storage.sync.get(["tabLimit"], function (result) {
@@ -13,21 +14,28 @@ document.addEventListener("DOMContentLoaded", function () {
         const newTabLimit = parseInt(tabLimitInput.value);
 
         if (!isNaN(newTabLimit)) {
-            if (newTabLimit <= 10) { // Ensure the new limit is not greater than 10
+            if (newTabLimit <= 15) {
                 // Save the new tab limit to storage
                 chrome.storage.sync.set({ tabLimit: newTabLimit }, function () {
                     // Notify the background script to apply the new limit
-                    chrome.runtime.sendMessage({ setTabLimit: newTabLimit });
 
-                    // Close the popup
-                    window.close();
+                    // Display the "Changes saved" message
+                    saveMessage.style.display = "block";
+                    saveMessage.textContent = "Changes saved.";
+
+                    // Close the popup after a delay
+                    setTimeout(function () {
+                        window.close();
+                    }, 5500);
                 });
             } else {
-                // Display a message indicating that the limit can't exceed 10
+                // Display a message indicating that the limit can't exceed 15 and set the text color to red
                 const status = document.getElementById("status");
-                status.textContent = "Maximum limit is 10 tabs.";
+                status.textContent = "Maximum limit is 15 tabs.";
+                status.style.color = "red"; // Set text color to red
                 setTimeout(function () {
                     status.textContent = "";
+                    status.style.color = ""; // Reset text color
                 }, 4500);
             }
         }
