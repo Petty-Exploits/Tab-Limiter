d99c85598037c2f862efd5b721d59fa389e7e494
Thomas Petty <146855548+Petty-Exploits@users.noreply.github.com>
2023-10-13 13:57:40 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,22 +1,23 @@
 let tabLimit = 10; // Default tab limit
-let windowLimit = 3; // Default window limit
 
-// Listen for messages from the popup or options page to set the tab and window limits
+// Listen for messages from the popup or options page to set the tab limit
 chrome.runtime.onMessage.addListener(function (request) {
-    if (request.setLimits) {
-        tabLimit = request.setLimits.tabLimit;
-        windowLimit = request.setLimits.windowLimit;
+    if (request.setTabLimit) {
+        tabLimit = request.setTabLimit;
     }
 });
 
 // Listen for tab creation events
 chrome.tabs.onCreated.addListener(function () {
-    enforceTabLimit(); // Call the function to enforce tab limits
+    enforceTabLimit();
 });
 
-// ...
+// Listen for tab removal events
+chrome.tabs.onRemoved.addListener(function () {
+    enforceTabLimit();
+});
 
-// Function to enforce the tab limit by closing excess tabs
+// Enforce the tab limit by closing excess tabs
 function enforceTabLimit() {
     chrome.tabs.query({}, function (tabs) {
         if (tabs.length > tabLimit) {
@@ -27,5 +28,3 @@ function enforceTabLimit() {
         }
     });
 }
-
-// The rest of your code for window limits and other functionality
