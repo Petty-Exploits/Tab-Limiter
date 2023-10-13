063403fea43f397a0b73a9920ef6eb43023a7661
Thomas Petty <146855548+Petty-Exploits@users.noreply.github.com>
2023-10-13 10:09:36 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,30 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tabLimitInput = document.getElementById("tabLimit");
+    const windowLimitInput = document.getElementById("windowLimit");
     const setLimitButton = document.getElementById("setLimitButton");
 
-    // Load the current tab limit from storage and display it
-    chrome.storage.sync.get(["tabLimit"], function (result) {
+    // Load the current tab and window limits from storage and display them
+    chrome.storage.sync.get(["tabLimit", "windowLimit"], function (result) {
         tabLimitInput.value = result.tabLimit || 10;
+        windowLimitInput.value = result.windowLimit || 3;
     });
 
     // Handle the "Set Limit" button click
     setLimitButton.addEventListener("click", function () {
         const newTabLimit = parseInt(tabLimitInput.value);
+        const newWindowLimit = parseInt(windowLimitInput.value);
 
-        if (!isNaN(newTabLimit)) {
-            if (newTabLimit <= 10) { // Ensure the new limit is not greater than 10
-                // Save the new tab limit to storage
-                chrome.storage.sync.set({ tabLimit: newTabLimit }, function () {
-                    // Notify the background script to apply the new limit
-                    chrome.runtime.sendMessage({ setTabLimit: newTabLimit });
+        if (!isNaN(newTabLimit) && !isNaN(newWindowLimit)) {
+            if (newTabLimit <= 10 && newWindowLimit <= 3) {
+                // Save the new tab and window limits to storage
+                chrome.storage.sync.set({ tabLimit: newTabLimit, windowLimit: newWindowLimit }, function () {
+                    // Notify the background script to apply the new limits
+                    chrome.runtime.sendMessage({ setLimits: { tabLimit: newTabLimit, windowLimit: newWindowLimit } });
 
                     // Close the popup
                     window.close();
                 });
             } else {
-                // Display a message indicating that the limit can't exceed 10
+                // Display a "Nice try" message when the limits are exceeded
                 const status = document.getElementById("status");
-                status.textContent = "Maximum limit is 10 tabs.";
+                if (newTabLimit > 10) {
+                    status.textContent = "Nice try! The maximum tab limit is 10.";
+                } else {
+                    status.textContent = "Nice try! The maximum window limit is 3.";
+                }
                 setTimeout(function () {
                     status.textContent = "";
                 }, 4500);
