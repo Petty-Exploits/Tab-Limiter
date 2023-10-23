7a128d73bf7ffc93528625d48b1490134f3392d4
Thomas Petty <146855548+Petty-Exploits@users.noreply.github.com>
2023-10-23 14:19:51 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,34 +1,70 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tabLimitInput = document.getElementById("tabLimit");
     const setLimitButton = document.getElementById("setLimitButton");
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const saveMessage = document.getElementById("saveMessage"); // Get the "Changes saved" message element
 
     // Load the current tab limit from storage and display it
     chrome.storage.sync.get(["tabLimit"], function (result) {
-        tabLimitInput.value = result.tabLimit || 10;
+        const currentTabLimit = result.tabLimit || 10;
+        tabLimitInput.value = currentTabLimit;
     });
+    // Get the version number from the manifest file
+    fetch(chrome.runtime.getURL("manifest.json"))
+        .then((response) => response.json())
+        .then((data) => {
+            const versionElement = document.getElementById("version");
+            versionElement.textContent = "Version: " + data.version;
+        });
 
     // Handle the "Set Limit" button click
     setLimitButton.addEventListener("click", function () {
         const newTabLimit = parseInt(tabLimitInput.value);
 
         if (!isNaN(newTabLimit)) {
-            if (newTabLimit <= 10) { // Ensure the new limit is not greater than 10
+            if (newTabLimit <= 15) {
                 // Save the new tab limit to storage
                 chrome.storage.sync.set({ tabLimit: newTabLimit }, function () {
                     // Notify the background script to apply the new limit
                     chrome.runtime.sendMessage({ setTabLimit: newTabLimit });
 
-                    // Close the popup
-                    window.close();
+                    // Display the "Changes saved" message
+                    saveMessage.textContent = "Changes saved.";
+                    saveMessage.style.display = "block";
+
+                    // Close the popup after a delay
+                    setTimeout(function () {
+                        window.close();
+                    }, 2000);
                 });
             } else {
-                // Display a message indicating that the limit can't exceed 10
-                const status = document.getElementById("status");
-                status.textContent = "Maximum limit is 10 tabs.";
-                setTimeout(function () {
-                    status.textContent = "";
-                }, 4500);
+                // Display a message indicating that the limit can't exceed 15
+                saveMessage.textContent = "Maximum limit is 15 tabs.";
+                saveMessage.style.color = "red"; // Set text color to red
+                saveMessage.style.display = "block";
             }
         }
     });
+
+    // Add dark mode functionality
+    chrome.storage.sync.get("darkMode", function (result) {
+        const isDarkMode = result.darkMode;
+        toggleDarkMode(isDarkMode);
+    });
+
+    darkModeToggle.addEventListener("click", function () {
+        chrome.storage.sync.get("darkMode", function (result) {
+            const isDarkMode = !result.darkMode;
+            toggleDarkMode(isDarkMode);
+            chrome.storage.sync.set({ darkMode: isDarkMode });
+        });
+    });
+
+    function toggleDarkMode(isDarkMode) {
+        if (isDarkMode) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    }
 });
