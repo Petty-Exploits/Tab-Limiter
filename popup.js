998448f071222fe9a07e738f74475a3aac013813
Thomas Petty <146855548+Petty-Exploits@users.noreply.github.com>
2023-10-30 13:23:28 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -10,19 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
         tabLimitInput.value = currentTabLimit;
     });
 
-    // Handle input events in the tabLimitInput
-    tabLimitInput.addEventListener("input", function () {
-        // Get the input value
-        const inputText = tabLimitInput.value.trim();
-
-        setLimitButton.disabled = false; // Enable the "Set Limit" button by default
-
-        if (inputText.toLowerCase() !== "konami" && inputText.toLowerCase() !== "suredoes") {
-            // If it's not a cheat code, check if it's a valid number
-            const newTabLimit = parseInt(inputText);
-            setLimitButton.disabled = isNaN(newTabLimit) || newTabLimit > 15;
-        }
-    });
+    // Hide the message initially
+    saveMessage.style.display = "none";
 
     // Handle the "Set Limit" button click
     setLimitButton.addEventListener("click", function () {
@@ -31,25 +20,39 @@ document.addEventListener("DOMContentLoaded", function () {
         if (newTabLimit === "konami") {
             // If the Konami code is entered, set the tab limit to 20
             setTabLimit(20, "Cheat code activated. Tab limit set to 20.");
-        } else if (newTabLimit === "suredoes") {
+            // Display the message and add the class to enable animation
+            saveMessage.textContent = "Cheat code activated. Tab limit set to 20.";
+            saveMessage.style.display = "block";
+            saveMessage.classList.add("cheat-code-message");
+        } else if (newTabLimit === "CHEATCODE") {
             // If the Cheat Code is entered, set the tab limit to 30
             setTabLimit(30, "Cheat code activated. Tab limit set to 30.");
+            // Display the message and add the class to enable animation
+            saveMessage.textContent = "Cheat code activated. Tab limit set to 30.";
+            saveMessage.style.display = "block";
+            saveMessage.classList.add("cheat-code-message");
         } else {
             // Check if the entered value is a valid number and within the maximum limit
             const numericTabLimit = parseInt(newTabLimit);
 
             if (!isNaN(numericTabLimit) && numericTabLimit <= 15) {
                 setTabLimit(numericTabLimit, "Changes saved.");
+                // Display the message and remove the animation class
+                saveMessage.textContent = "Changes saved.";
+                saveMessage.style.display = "block";
+                saveMessage.classList.remove("cheat-code-message");
             } else {
                 // Display a message indicating that the limit can't exceed 15
                 saveMessage.textContent = "Maximum limit is 15 tabs.";
-                saveMessage.style.color = "red"; // Set text color to red
+                saveMessage.style.color = "red";
                 saveMessage.style.display = "block";
+                // Remove the animation class
+                saveMessage.classList.remove("cheat-code-message");
             }
         }
     });
 
-    // Add dark mode functionality (this part remains the same)
+    // Add dark mode functionality
     chrome.storage.sync.get("darkMode", function (result) {
         const isDarkMode = result.darkMode;
         toggleDarkMode(isDarkMode);
@@ -79,13 +82,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Display the message
             saveMessage.textContent = message;
-            saveMessage.style.color = "green"; // Set text color to green
+            saveMessage.style.color = "green";
             saveMessage.style.display = "block";
 
             // Close the popup after a delay
             setTimeout(function () {
                 window.close();
-            }, 5000);
+            }, 10000);
         });
     }
 });
